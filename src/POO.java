6d66eb1a9657e26312e6b5141d2a6fb8598b6d3d
Diego <d.caceres02@ufromail.cl>
2023-03-20 22:51:29 -0300

    commit estudiante 2

---
+++ src/POO.java
@@ -0,0 +1,11 @@
+public class POO {
+    public void metodoA(){
+        System.out.println("muestra algo");
+    }
+    public int obtenernumero(int numero){
+        return numero;
+    }
+    public String obtenerTexto(){
+        return "Hola Gente";
+    }
+}
